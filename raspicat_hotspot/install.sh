2e1cee3f915184265e3bdd1451960178aa6bd95e
Tatsuhiro Ikebe <beike315@icloud.com>
2022-06-16 15:14:30 +0900

    Update raspicat_hotspot (#4)

--- raspicat_hotspot/install.sh
+++ raspicat_hotspot/install.sh
@@ -5,6 +5,7 @@ git clone https://github.com/oblique/create_ap
 cd create_ap
 sudo make install
 cd -
-sudo ln -s $(rospack find raspicat_hotspot)/raspicat_hotspot.service /etc/systemd/system/raspicat_hotspot.service
+rm -rf create_ap
+sudo ln -s $(rospack find raspicat_hotspot)/service/raspicat_hotspot.service /etc/systemd/system/raspicat_hotspot.service
 sudo systemctl enable raspicat_hotspot.service
 sudo systemctl start raspicat_hotspot.service
