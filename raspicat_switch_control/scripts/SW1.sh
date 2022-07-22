ea5f2b378190badba0f5d805eb45f1debb13b67c
Tatsuhiro Ikebe <beike315@icloud.com>
2022-07-22 14:26:50 +0900

    Update SW1.sh (#12)

--- raspicat_switch_control/scripts/SW1.sh
+++ raspicat_switch_control/scripts/SW1.sh
@@ -2,21 +2,21 @@
 
 # waypoint_navigation_start
 
+# When the switch is off
+if [ $1 = "off" ] || [ -n "$count" ]; then
+  echo restart_waypoint_navigation
+  rosservice call --wait /way_nav_restart & 
+  led_off 2
+  sleep 2
+fi
+########################
+
 # When the switch is on
 if [ $1 = "on" ] && [ -z "$count" ]; then
   echo start_waypoint_navigation
-  rostopic pub -1 /way_nav_start std_msgs/Empty
+	rosservice call --wait /way_nav_start & 
   led_on 2
   count=0
   sleep 2
 fi
 #######################
-
-# When the switch is off
-if [ $1 = "off" ] || [ -n "$count" ]; then
-  echo restart_waypoint_navigation
-  rostopic pub -1 /way_nav_restart std_msgs/Empty
-  led_off 2
-  sleep 2
-fi
-########################
