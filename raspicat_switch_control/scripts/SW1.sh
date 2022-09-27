f7b5669145affc576c77c5fcbeaeb5987e684914
Tatsuhiro Ikebe <beike315@icloud.com>
2022-09-27 19:52:17 +0900

    Fix (#14)

--- raspicat_switch_control/scripts/SW1.sh
+++ raspicat_switch_control/scripts/SW1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# waypoint_navigation_start
+# waypoint_navigation_restart
 
 # When the switch is off
 if [ $1 = "off" ] || [ -n "$count" ]; then
@@ -11,12 +11,12 @@ if [ $1 = "off" ] || [ -n "$count" ]; then
 fi
 ########################
 
-# waypoint_navigation_restart
+# waypoint_navigation_start
 
 # When the switch is on
 if [ $1 = "on" ] && [ -z "$count" ]; then
   echo start_waypoint_navigation
-	rosservice call --wait /way_nav_start & 
+  rosservice call --wait /way_nav_start & 
   led_on 2
   count=0
   sleep 2
