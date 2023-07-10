b514656e58e7c55ce784396b458982ffadec0c93
Tatsuhiro Ikebe <beike315@icloud.com>
2023-07-10 16:59:57 +0900

    Feature time synchronization offline

--- time_synchronization/scripts/setup_raspi.sh
+++ time_synchronization/scripts/setup_raspi.sh
@@ -3,3 +3,5 @@
 sudo apt install -y chrony
 
 sudo timedatectl set-timezone Asia/Tokyo
+
+sudo bash -c 'echo -e "\nserver 10.42.0.1 minpoll 0 maxpoll 5\nserver 192.168.12.12 minpoll 0 maxpoll 5\nmakestep 10 3" >> /etc/chrony/chrony.conf'
--- time_synchronization/scripts/setup_remote_pc.sh
+++ time_synchronization/scripts/setup_remote_pc.sh
@@ -2,6 +2,6 @@
 
 sudo apt install -y chrony
 
-sudo bash -c 'echo -e "\nallow 192.168.0/24\nallow 10.42/24" >> /etc/chrony/chrony.conf'
+sudo bash -c 'echo -e "\nlocal stratum 10\nallow 192.168.12/24\nallow 10.42/24" >> /etc/chrony/chrony.conf'
 
 sudo systemctl restart chrony.service
